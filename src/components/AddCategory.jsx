96c4eb5b51d5d8619ca6c7fc5cdfd16de225b27a
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-09-13 22:48:55 -0300

    adds tests

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState("");
@@ -14,7 +15,7 @@ export const AddCategory = ({ onNewCategory }) => {
     onNewCategory(inputValue.trim());
   };
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <input
         type="text"
         placeholder="Buscar gifs"
@@ -23,4 +24,8 @@ export const AddCategory = ({ onNewCategory }) => {
       />
     </form>
   );
+};
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
 };
